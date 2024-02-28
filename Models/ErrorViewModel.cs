df4079023c8c98b4a20730c093da2154a7a1c8f8
Radhika <radhikasn06@gmail.com>
2024-02-28 09:38:14 +0530

    success

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phase4PizzaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/OrderInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Phase4PizzaApp.Models
+{
+    
+        public class OrderInfo : Pizza
+        {
+            public int OrderId { get; set; }
+
+            public int TotalPrice { get; set; }
+        }
+    }
+
